0a7dd8975a9ece054a9f4a94c13cf909eb12b8c3
CarlosMandele <carlos.mandelle@gmail.com>
2024-01-01 21:01:16 +0000

    Add new comments

--- datascraping.R
+++ datascraping.R
@@ -1,8 +1,8 @@
-#-----------------------------------------------------------#
-# TELECHARGEMENT, INSTALLATION ET ACTIVATION DES LIBRAIRIES #
-#-----------------------------------------------------------#
+#------------------------------------------------#
+# DOWNLOAD, INSTALAÇÃO E ATIVAÇÃO DE BIBLIOTECAS #
+#------------------------------------------------#
 
-# Installation des librairies (mise-a-jour si d�j� install�es)
+# Instalação de bibliotecas (atualização se já estiver instalada)
 install.packages("rvest")
 install.packages("ggplot2")
 install.packages("dplyr")
@@ -11,7 +11,7 @@ install.packages("maps")
 install.packages("mapproj")
 install.packages("plotly")
 
-# Activation des librairies
+# Ativação de bibliotecas
 library(rvest)
 library(ggplot2)
 library(dplyr)
@@ -21,68 +21,67 @@ library(mapproj)
 library(plotly)
 library(data.table)
 
-#-----------------------------------------#
-# CHARGEMENT DES DONNEES DEPUIS WIKIPEDIA #
-#-----------------------------------------#
+#-------------------------#
+# CARREGANDO DADOS DA WEB #
+#-------------------------#
 
-# Chargement du code HTML de la page Internet dans un objet nomme 'le' avec la  fonction 'read_html()' 
+# Carregando o código HTML da página Web 
 le = read_html("https://en.wikipedia.org/w/index.php?title=List_of_U.S._states_and_territories_by_life_expectancy&ol-did=928537169")
 
-# Filtrage du code HTML dans 'le' afin de ne conserver que la table sur les esp�rances de vie
+# Filtrando o código HTML na variavel 'le' para manter apenas a tabela sobre as expectativas de vida
 le = le %>% html_nodes("table") %>% .[[2]] %>% html_table(fill=T)
 View(le)
 
 #-------------------------#
-# PREPARATION DES DONNEES #
+# PROCESSAMENTO DOS DADOS #
 #-------------------------#
 
-# S�lection des colonnes utilis�es
+# Seleção das colunas utilizadas
 le = le[c(1,2,6,7)]
 View(le)
 
-# Renommage des colonnes ('le' = life expectancy)
+# Renomeando as colunas ("life expectancy")
 names(le)[c(3,4)] = c("caucasiano", "africano")
 View(le)
 
-# Conversion des colonnes au format num�rique (valeurs manquantes cod�es NA)
+# Convertendo colunas para formato numérico (valores ausentes codificados NA)
 le$caucasiano <- as.numeric(le$caucasiano)
 le$africano <- as.numeric(le$africano)
 View(le)
 
-# Calcul des diff�rences entre esp�rance de vie Caucasienne-Am�ricaine et Afro-Am�ricaine 
+# Cálculo das diferenças entre a esperança de vida caucasiana-americana e afro-americana
 le$difer = le$caucasiano - le$africano
 View(le)
-
-# Chargement de la liste des �tats des USA dans un objet 'states'
+#
 states = map_data("state")
 
-# Cr�ation d'une nouvelle variable 'region' avec des noms des �tats 
+# Nova variável 'region' com nomes dos estados 
 le$region = tolower(le$`State/Territory`)
 View(le)
 
-# Fusion des donn�es de 'le' avec celles des �tats-Unis dans l'objet 'states'
+# Mesclando dados 'le' e 'states' no objeto 'states'
 states = merge(states, le, by="region", all.x=T)
 
-#---------------------------------------------------#
-# AFFICHAGE DES DONNEES SUR LA CARTE DES ETATS-UNIS #
-#---------------------------------------------------#
+#-----------------------#
+# VISUALIZAÇÃO DOS DADOS #
+#-----------------------#
 
-# Affichage des caract�res accentu�s dans les cartes
+# Algumas configuração(se necessáro)
 options(encoding="latin1")
 
-# Affichage des esp�rance de vie du groupe ethnique Afro-Am�ricains
+# Visualização das expectativas de vida dos grupos étnicos afro-americanos
 ggplot(states, aes(x = long, y = lat, group = group, fill = africano)) + geom_polygon(color = "white") + scale_fill_gradient(name = "Years", low = "#ffe8ee", high = "#c81f49", guide = "colorbar", na.value="#eeeeee") + labs(title="Esperança de vida do grupo étnico afro-americano") + coord_map()
 
-# Affichage des esp�rance de vie du groupe ethnique Caucasien-Am�ricains
+# #
 ggplot(states, aes(x = long, y = lat, group = group, fill = caucasiano)) + geom_polygon(color = "white") + scale_fill_gradient(name = "Years", low = "#ffe8ee", high = "#c81f49", guide = "colorbar", na.value="Gray") + labs(title="Esperança de vida do grupo étnico caucasiano-americano") + coord_map()
 
-# Affichage des disparit�s entre les esp�rances de vie des deux groupes ethniques
+# Disparidades entre as expectativas de vida dos dois grupos étnicos
 ggplot(states, aes(x = long, y = lat, group = group, fill = difer)) + geom_polygon(color = "white") + scale_fill_gradient(name = "Years", low = "#ffe8ee", high = "#c81f49", guide = "colorbar", na.value="#eeeeee") + labs(title="Diferenças na esperança de Vida dos grupos étnicos caucasianos e afro-americanos por estado nos EUA") + coord_map()
 
-# Affichage d'une carte interactive des USA map avec message contextuel pour chaque �tat : Caucasien-Am�ricains
+# Gerando um mapa interativo do mapa dos EUA com mensagem contextual para cada estado: Caucasianos-americanos
 map_plot_caucasian = ggplot(states, aes(x = long, y = lat, group = group, fill = caucasiano)) + geom_polygon(color = "white") + scale_fill_gradient(name = "Years", low = "#ffe8ee", high = "#c81f49", guide = "colorbar", na.value="#eeeeee") + labs(title="Esperança de vida do grupo étnico caucasiano-americano") + coord_map()
 ggplotly(map_plot_caucasian)
 
-# # Affichage d'une carte interactive des USA map avec message contextuel pour chaque �tat : Afro-Am�ricains
+# #
 map_plot_african = ggplot(states, aes(x = long, y = lat, group = group, fill = africano)) + geom_polygon(color = "white") + scale_fill_gradient(name = "Years", low = "#ffe8ee", high = "#c81f49", guide = "colorbar", na.value="#eeeeee") + labs(title="Esperança de vida do grupo étnico afro-americano") + coord_map()
 ggplotly(map_plot_african)
